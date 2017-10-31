f9b5a9ade296bf081da7806a6118378de2f9663e
Rileygowan <t.rileygowan@gmail.com>
2017-10-31 00:29:46 +0000

    Setting up the styles and first components for ChattyApp

---
+++ src/Message.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    console.log('Rendering <Message/>')
+    return (
+      <span>
+      <div className="message">
+        <span className="message-username">Anonymous1</span>
+        <span className="message-content">I won&#39;t be impressed with technology until I can download food.</span>
+      </div>
+      <div className="message system">
+        Anonymous1 changed their name to nomnom.
+      </div>
+      </span>
+    );
+  }
+}
+export default Message;
